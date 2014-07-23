b0da4f9d0b9a320af6e09072b2d8560792d9c284
Ben Brougher <moutansos@gmail.com>
2014-07-22 20:45:51 -0700

    New zip method. Should work better for backing up files.

--- main.py
+++ main.py
@@ -28,10 +28,43 @@ class M (object):
 	def empty(self):
 		print "Unimplimented option."
 
-	def zipdir(self, path, zip):
-	    for root, dirs, files in os.walk(path):
-	    	for file in files:
-	    		zip.write(os.path.join(root, file))
+	def zip_folder(folder_path, output_path):
+	    """Zip the contents of an entire folder (with that folder included
+	    in the archive). Empty subfolders will be included in the archive
+	    as well.
+	    """
+	    parent_folder = os.path.dirname(folder_path)
+	    # Retrieve the paths of the folder contents.
+	    contents = os.walk(folder_path)
+	    try:
+	        zip_file = zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED)
+	        for root, folders, files in contents:
+	            # Include all subfolders, including empty ones.
+	            for folder_name in folders:
+	                absolute_path = os.path.join(root, folder_name)
+	                relative_path = absolute_path.replace(parent_folder + '\\',
+	                                                      '')
+	                print "Adding '%s' to archive." % absolute_path
+	                zip_file.write(absolute_path, relative_path)
+	            for file_name in files:
+	                absolute_path = os.path.join(root, file_name)
+	                relative_path = absolute_path.replace(parent_folder + '\\',
+	                                                      '')
+	                print "Adding '%s' to archive." % absolute_path
+	                zip_file.write(absolute_path, relative_path)
+	        print "'%s' created successfully." % output_path
+	    except IOError, message:
+	        print message
+	        sys.exit(1)
+	    except OSError, message:
+	        print message
+	        sys.exit(1)
+	    except zipfile.BadZipfile, message:
+	        print message
+	        sys.exit(1)
+	    finally:
+	        zip_file.close()
+
 
 	## BACKEND FUNCTIONS ====================================
 
